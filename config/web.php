7566d51d5aace9d94d67c7cd702e51dad880e5d4
JosueMorales93 <josue.morales@outlook.com>
2015-05-14 09:53:51 -0600

    Primer Commit

---
+++ config/web.php
@@ -0,0 +1,66 @@
+<?php
+
+$params = require(__DIR__ . '/params.php');
+
+$config = [
+    'id' => 'BugTracker',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'c00ki3',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => require(__DIR__ . '/db.php'),
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            // Disable index.php
+            'showScriptName' => false,
+            // Disable r= routes
+            'enablePrettyUrl' => true,
+            'rules' => array(
+                    '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                    '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                    '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ),
+        ],
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
---
+++ models/Usuario.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "usuario".
+ *
+ * @property integer $Id
+ * @property string $Nombre
+ * @property string $Apellido
+ * @property string $Usuario
+ * @property string $Password
+ * @property integer $IdRol
+ * @property string $Email
+ * @property integer $Activo
+ * @property integer $ResetPassword
+ *
+ * @property Rol $idRol
+ */
+class Usuario extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'usuario';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['Nombre', 'Usuario', 'Password', 'IdRol', 'Email', 'Activo', 'ResetPassword'], 'required'],
+            [['IdRol', 'Activo', 'ResetPassword'], 'integer'],
+            [['Nombre', 'Apellido'], 'string', 'max' => 100],
+            [['Usuario'], 'string', 'max' => 45],
+            [['Password'], 'string', 'max' => 500],
+            [['Email'], 'string', 'max' => 200]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'Id' => 'ID',
+            'Nombre' => 'Nombre',
+            'Apellido' => 'Apellido',
+            'Usuario' => 'Usuario',
+            'Password' => 'Password',
+            'IdRol' => 'Id Rol',
+            'Email' => 'Email',
+            'Activo' => 'Activo',
+            'ResetPassword' => 'Reset Password',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getIdRol()
+    {
+        return $this->hasOne(Rol::className(), ['Id' => 'IdRol']);
+    }
+}
